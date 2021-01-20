2b7657dfb1a1dd4ec47f462dd51d1eee535b9100
Cevahir <Cevahir@CEVAHIR>
2021-01-20 20:55:40 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Kayıt İşleminiz Oluşturuldu. Sayın: " + musteri.MusteriAdi + " " + musteri.MusteriSoyadi);
+        }
+
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine("Silme İşleminiz Gerçekleştirildi. Sayın: " + musteri.MusteriAdi + " " + musteri.MusteriSoyadi);
+        }
+        public void Liste(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi+" "+ musteri.Bakiye);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.ID = 1;
+            musteri1.MusteriAdi = "Emre Cevahir";
+            musteri1.MusteriSoyadi = "Korkmaz";
+            musteri1.Bakiye = 100;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.ID = 1;
+            musteri2.MusteriAdi = "Çiko";
+            musteri2.MusteriSoyadi = "Korkmaz";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Silme(musteri1);
+            musteriManager.Silme(musteri2);
+            musteriManager.Liste(musteriler);
+
+        }
+    }
+}
